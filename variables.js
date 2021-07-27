32fe51a54e5dbe062c39d53983fdbf535320c0fb
ojsilvera <ojsilvera@gmail.com>
2021-07-27 10:24:22 -0500

    declarando variables

---
+++ variables.js
@@ -0,0 +1,25 @@
+// Se declara la variable o reserva
+var nombre;
+
+// se le asigna un valor y por ende un tipo de dato
+nombre = 30;
+
+//se declara y asigna el valor
+var numero = 50;
+
+// estring
+var elementos = ["pc", "mouse"];
+
+// para obtener un dato del arreglo anterior es como se muestra en la siguiente linea
+// su salida sera mouse
+elemento[1];
+
+//objeto
+var personas = {
+    nombre: "Oscar",
+
+    edada: 39
+};
+
+
+
