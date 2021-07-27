0c6d19509814450531d4d9f8e41c04abdc40d04a
ojsilvera <ojsilvera@gmail.com>
2021-07-27 14:40:38 -0500

    switch

---
+++ switch.js
@@ -0,0 +1,27 @@
+// var
+// switch(var){
+//     case var == var:
+//         aqui lo que sucede si es verdad;
+//         break
+//     case var == var:
+//         aqui lo que sucede si es verdad
+//         break
+//     case var == var:
+//         aqui lo que sucede si es verdad
+//         break
+//      default:
+//         aqui sino se cumple ninguno
+// }
+
+var numero = 1;
+
+switch(numero){
+    case 1:
+        console.log("correcto soy un 1");
+        break
+    case 10:
+        console.log("correcto soy un 10");
+        break
+    default:
+        console.log("no me has encontrado aun");
+}
