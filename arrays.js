aeedb315c50f07869155f566bf64f6aa6c5c0ee9
ojsilvera <ojsilvera@gmail.com>
2021-07-28 13:38:37 -0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var frutas = ["manzana", "pera", "papaya", "melon"]
+
+// llamado en cosola al arreglo
+console.log(frutas);
+
+// cantidad de elementos de un arreglo
+console.log(frutas,length);
+
+// llamado en cosola un elemento del arreglo
+//javascript inicia el conteo en 0
+console.log(frutas[2]);
+
+// agregar elementos al final del arreglo
+var masFrutas =  frutas.push("uvas")
+
+// eliminar ultimo elmento del arreglo
+var masFrutas =  frutas.pop("uvas")
+
+// agregar elementos al principio del arreglo
+var masFrutas =  frutas.unshift("coco")
+
+// leimina el primer elemento del arreglo
+var masFrutas =  frutas.shift("coco")
+
+// leimina el primer elemento del arreglo
+varpositions = frutas.indexOf("melon")
