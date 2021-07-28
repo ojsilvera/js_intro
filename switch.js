5da3efc5553007fab4d6090065087fe9febac8be
ojsilvera <ojsilvera@gmail.com>
2021-07-28 12:39:31 -0500

    switch piedra pael o tijeras

--- switch.js
+++ switch.js
@@ -25,3 +25,50 @@ switch(numero){
     default:
         console.log("no me has encontrado aun");
 }
+
+
+
+// ejemplo piedra papel o tijera
+
+var opc = ["tijera", "piedra",  "papel"];
+
+var opcMaquina = opc[ Math.floor((Math.random() * (3-0))+0) ];
+
+var opcJugador = opc[ Math.floor((Math.random() * (3-0))+0) ];
+
+
+function juego(opcMaquina, opcJugador){
+
+    op = opcMaquina + opcJugador
+
+    switch(op){
+
+//casos en los que gana el player
+        case  "piedrapapel":
+            console.log("gana Jugador");
+            break
+        case  "papeltijera":
+            console.log("gana Jugador");
+            break
+        case  "tijerapiedra":
+            console.log("gana Jugador");
+            break
+//casos en lo que gana la maquina
+        case  "tijerapapel":
+            console.log("gana Maquina");
+            break
+        case  "piedratijera":
+            console.log("gana Maquina");
+            break
+        case  "papelpiedra":
+            console.log("gana Maquina");
+            break
+//opcion empate
+        default:
+            console.log("Empatados");
+            break
+    }
+
+}
+
+juego(opcMaquina, opcJugador);
